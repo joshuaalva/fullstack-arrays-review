e4ac63fde231bbb223ed8d9f1b8dc493cec730da
Joshua Alvarado <joshalva3@gmail.com>
2022-12-25 14:25:22 -0600

    my last index of workshop problem no solution

--- index.js
+++ index.js
@@ -51,3 +51,73 @@
 // }
 
 // 02 My Includes
+
+// function myIncludes(arr, searchValue){
+//     for(let i = 0; i < arr.length; i++){
+//       let element = arr[i];
+//       if(element === searchValue){
+//         return true;
+//       }
+//     }
+//         return false;
+//   }
+  
+//   describe('myIncludes', () => {
+  
+//     it('is a function', () => {
+//       expect(typeof myIncludes).toEqual('function');
+//     });
+  
+//     it('returns a boolean', () => {
+//       let returnedValue = myIncludes([1, 2, 3], 3);
+//       expect(typeof returnedValue).toEqual('boolean');
+//     });
+  
+//     it('returns true if the searchValue is in the array', () => {
+//       let returnedValue = myIncludes(['ruby', 'go', 'javascript'], 'javascript');
+//       expect(returnedValue).toEqual(true);
+//     });
+  
+//     it('returns false if the searchValue is not in the array', () => {
+//       let returnedValue = myIncludes(['fun', 'happy things', 'kittens'], 'sad things');
+//       expect(returnedValue).toEqual(false);
+//     });
+  
+//     it('returns false if the searchValue is loosely equal to an element in the array', () => {
+//       let returnedValue = myIncludes([5, 10, 15], '10');
+//       expect(returnedValue).toEqual(false);
+//     });
+  
+//   });
+
+// 03 My Last Index Of
+
+describe('myLastIndexOf', () => {
+
+    it('is a function', () => {
+      expect(typeof myLastIndexOf).toEqual('function');
+    });
+  
+    it('returns a number', () => {
+      let returnedValue = myLastIndexOf([1, 2, 3], 2);
+      expect(typeof returnedValue).toEqual('number');
+    });
+  
+    it('returns the last index at which the searchValue appears in the array', () => {
+      let returnedValue = myLastIndexOf([0, 10, 20, 10, 0], 10);
+      expect(returnedValue).toEqual(3);
+    });
+  
+    it('returns -1 if the searchValue does not exist in the array', () => {
+      let returnedValue = myLastIndexOf(['peanut', 'cashew', 'walnut'], 'lemon');
+      expect(returnedValue).toEqual(-1);
+    });
+  
+    it('returns the last index of the searchValue that is less than the startIdx', () => {
+      let returnedValue = myLastIndexOf([0, 10, 20, 10, 0], 10, 2);
+      expect(returnedValue).toEqual(1);
+    });
+  
+  });
+
+  
