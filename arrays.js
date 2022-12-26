78b8c2dda48d464b25cbc6472ddb79d36c0b0056
Joshua Alvarado <joshalva3@gmail.com>
2022-12-26 12:43:06 -0600

    arrays 2 review start

---
+++ arrays.js
@@ -0,0 +1,9 @@
+// two dimensional arrays 
+
+const aisles = [
+    ['bread', 'hamburger buns', 'hotdog buns'],
+    ['milk', 'cheese'],
+    ['tomatoes', 'onions', 'lettice']
+];
+
+console.log(aisles[1][0]);
