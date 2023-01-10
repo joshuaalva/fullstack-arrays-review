c9137a3824b7507faf7dca6f9fb45675b1eb5445
Joshua Alvarado <joshalva3@gmail.com>
2023-01-10 05:07:25 -0600

    even and odd solution start of 02 array flattener

--- arraysworkshop.js
+++ arraysworkshop.js
@@ -1,7 +1,21 @@
 // 01 Even and Odd 
-function evenAndOdd(arr) {
+function evenAndOdd(arr){
+    // create arrays to store even and odd numbers
+    let evenArr = [];
+    let oddArr = [];
     
-}
+    for(let i = 0; i < arr.length; i++){
+      let num = arr[i];
+      
+      if(num % 2){
+        oddArr.push(num);
+      } else {
+        evenArr.push(num);
+      }
+    }
+    
+    return [evenArr, oddArr];
+  };
 
 describe('evenAndOdd', () => {
 
@@ -25,4 +39,44 @@ describe('evenAndOdd', () => {
     });
   
   });
+
+  // 02 Array Flattner 
+
+  describe('arrayFlattener', () => {
+
+    it('is a function', () => {
+      expect(typeof arrayFlattener).toEqual('function');
+    });
+  
+    it('returns an array', () => {
+      let returnedValue = arrayFlattener([1, 2, [3]]);
+      expect(Array.isArray(returnedValue)).toEqual(true);
+    });
+  
+    it('returns a one-dimensional array', () => {
+      // get the return value from the function
+      let returnedValue = arrayFlattener('this is crazy');
+  
+      // make sure none of the elements in the array are an array
+      let foundInnerArray = false;
+  
+      for (let i = 0; i < returnedValue.length; i++) {
+        let element = returnedValue[i];
+  
+        if (Array.isArray(element)) {
+          foundInnerArray = true;
+          break;
+        }
+      }
+  
+      expect(foundInnerArray).toEqual(false);
+    });
+  
+    it('returns a one-dimensional array with the correct elements', () => {
+      let returnedValue = arrayFlattener(['I', 'am', 'working', ['on', 'another', 'level']]);
+      expect(returnedValue).toEqual(['I', 'am', 'working', 'on', 'another', 'level']);
+    });
+  
+  });
+  
   
